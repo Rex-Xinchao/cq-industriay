6ca66d63b6ffae7543b0461a940aeb2138682935
Rex-Xinchao <13601935905@163.com>
2021-01-13 10:45:54 +0800

    update

--- frontend/src/components/stockCustom/branch/external_risk_table.vue
+++ frontend/src/components/stockCustom/branch/external_risk_table.vue
@@ -9,7 +9,7 @@
           <span class="icon icon_1"></span>
           <span class="line">
             <p>
-              <b>25</b>
+              <b>{{ num_1 }}</b>
               家企业
             </p>
             <p>裁判文书</p>
@@ -21,7 +21,7 @@
           <span class="icon icon_2"></span>
           <span class="line">
             <p>
-              <b>25</b>
+              <b>{{ num_2 }}</b>
               家企业
             </p>
             <p>失信被执行人</p>
@@ -33,7 +33,7 @@
           <span class="icon icon_3"></span>
           <span class="line">
             <p>
-              <b>25</b>
+              <b>{{ num_3 }}</b>
               家企业
             </p>
             <p>被执行人</p>
@@ -45,7 +45,7 @@
           <span class="icon icon_4"></span>
           <span class="line">
             <p>
-              <b>25</b>
+              <b>{{ num_4 }}</b>
               家企业
             </p>
             <p>被执行人</p>
@@ -59,6 +59,7 @@
       :data="tableData"
       style="width: 100%"
       height="324px"
+      :id="tableId"
     >
       <el-table-column prop="comName" label="公司名称"></el-table-column>
       <el-table-column prop="buName" label="管护机构" width="80"></el-table-column>
@@ -75,21 +76,21 @@
       <el-table-column label="裁判文书" align="right">
         <template slot-scope="scope">
           <span class="active-text" @click="showDialog(1, scope.row)">
-            {{ scope.row.reason ? scope.row.reason + '起' : '--' }}
+            {{ scope.row.judgements ? scope.row.judgements + '起' : '--' }}
           </span>
         </template>
       </el-table-column>
       <el-table-column label="被执行情况" align="right">
         <template slot-scope="scope">
           <span class="active-text" @click="showDialog(2, scope.row)">
-            {{ scope.row.reason ? scope.row.reason + '起' : '--' }}
+            {{ scope.row.enforcements ? scope.row.enforcements + '起' : '--' }}
           </span>
         </template>
       </el-table-column>
       <el-table-column label="失信被执行情况" align="right">
         <template slot-scope="scope">
           <span class="active-text" @click="showDialog(3, scope.row)">
-            {{ scope.row.reason ? scope.row.reason + '起' : '--' }}
+            {{ scope.row.disEnforcements ? scope.row.disEnforcements + '起' : '--' }}
           </span>
         </template>
       </el-table-column>
@@ -120,9 +121,12 @@
 </template>
 
 <script>
+import { industry_risk, judgement, enforcement, dishonest_enforcement, shares_freeze } from '@/api/custom'
+import scroll from '@/mixins/scroll'
 export default {
   data() {
     return {
+      tableId: 'riskTable',
       dateTime: [],
       options: [
         {
@@ -148,11 +152,15 @@ export default {
       ],
       loading: false,
       loading_table: false,
-      tableData: [{}],
+      tableData: [],
       dialogData: [],
       tableConf: [],
       dialogTitle: null,
-      dialogVisible: false
+      dialogVisible: false,
+      num_1: 0,
+      num_2: 0,
+      num_3: 0,
+      num_4: 0
     }
   },
   props: {
@@ -167,10 +175,43 @@ export default {
       immediate: true,
       handler(data) {
         if (!data.length) return
+        this.getDate()
       }
     }
   },
+  mixins: [scroll],
   methods: {
+    getDate() {
+      this.loading = true
+      industry_risk({
+        industryCode: '',
+        buCode: '',
+        page: this.page.pageNo,
+        size: this.page.pageSize
+      })
+        .then((res) => {
+          this.loading = false
+          if (res.customers.length === 0) {
+            this.end = true
+          } else {
+            this.tableData.push(...res.customers)
+          }
+          this.noData = this.tableData.length === 0
+          this.num_1 = res.judgements
+          this.num_2 = res.enforcements
+          this.num_3 = res.disEnforcements
+          this.num_4 = res.freezes
+        })
+        .catch((err) => {
+          this.loading = false
+          this.noData = true
+          this.tableData = []
+          this.num_1 = 0
+          this.num_2 = 0
+          this.num_3 = 0
+          this.num_4 = 0
+        })
+    },
     handleClose() {
       this.dialogData = []
       this.dialogTitle = null
@@ -181,147 +222,167 @@ export default {
       this.tableConf = null
       this.dialogTitle = `${data.name} ${typeNameList[type - 1]}`
       this.dialogVisible = true
-      this[`getTypeData_${type}`]()
+      this[`getTypeData_${type}`](data.csfId)
     },
-    getTypeData_1() {
+    getTypeData_1(csfId) {
       this.tableConf = [
         {
           label: '案号',
-          prop: 'a'
+          prop: 'caseNumber'
         },
         {
           label: '案件类型',
-          prop: 'b'
+          prop: 'caseType'
         },
         {
           label: '法院',
-          prop: 'c'
+          prop: 'court'
         },
         {
           label: '标题',
-          prop: 'd'
+          prop: 'title'
         },
         {
           label: '原告',
-          prop: 'e'
+          prop: 'plaintiff'
         },
         {
           label: '被告',
-          prop: 'f'
+          prop: 'defendant'
         },
         {
           label: '案由',
-          prop: 'g'
+          prop: 'reason'
         },
         {
           label: '发布时间',
-          prop: 'h'
+          prop: 'releaseTime'
         }
       ]
       this.loading_table = true
-      setTimeout(() => {
-        this.dialogData = []
-        this.loading_table = false
-      }, 500)
+      judgement({ companyId: csfId })
+        .then((res) => {
+          this.dialogData = res.result || []
+          this.loading_table = false
+        })
+        .catch((err) => {
+          this.dialogData = []
+          this.loading_table = false
+        })
     },
-    getTypeData_2() {
+    getTypeData_2(csfId) {
       this.tableConf = [
         {
           label: '被执行人',
-          prop: 'a'
+          prop: 'companyName'
         },
         {
           label: '案号',
-          prop: 'b'
+          prop: 'caseNumber'
         },
         {
           label: '执行法院',
-          prop: 'c'
+          prop: 'court'
         },
         {
           label: '执行标的（元）',
-          prop: 'd'
+          prop: 'executionSubject'
         },
         {
           label: '立案时间',
-          prop: 'e'
+          prop: 'filingTime'
         }
       ]
       this.loading_table = true
-      setTimeout(() => {
-        this.dialogData = []
-        this.loading_table = false
-      }, 500)
+      enforcement({ companyId: csfId })
+        .then((res) => {
+          this.dialogData = res.result || []
+          this.loading_table = false
+        })
+        .catch((err) => {
+          this.dialogData = []
+          this.loading_table = false
+        })
     },
-    getTypeData_3() {
+    getTypeData_3(csfId) {
       this.tableConf = [
         {
           label: '失信被执行人',
-          prop: 'a'
+          prop: 'companyName'
         },
         {
           label: '执行依据文号',
-          prop: 'b'
+          prop: 'caseNumber'
         },
         {
           label: '所属法院',
-          prop: 'c'
+          prop: 'court'
         },
         {
           label: '生效法律文书确认义务',
-          prop: 'd'
+          prop: 'caseContent'
         },
         {
           label: '履行情况',
-          prop: 'e'
+          prop: 'impleProcess'
         },
         {
           label: '失信行为',
-          prop: 'f'
+          prop: 'dishonestBehavior'
         },
         {
           label: '发布时间',
-          prop: 'h'
+          prop: 'releaseTime'
         }
       ]
       this.loading_table = true
-      setTimeout(() => {
-        this.dialogData = []
-        this.loading_table = false
-      }, 500)
+      dishonest_enforcement({ companyId: csfId })
+        .then((res) => {
+          this.dialogData = res.result || []
+          this.loading_table = false
+        })
+        .catch((err) => {
+          this.dialogData = []
+          this.loading_table = false
+        })
     },
-    getTypeData_4() {
+    getTypeData_4(csfId) {
       this.tableConf = [
         {
           label: '股权被冻结方',
-          prop: 'a'
+          prop: 'companyName1'
         },
         {
           label: '冻结股权标的企业',
-          prop: 'b'
+          prop: 'companyName2'
         },
         {
           label: '执行法院',
-          prop: 'c'
+          prop: 'court'
         },
         {
           label: '执行裁定文书号',
-          prop: 'd'
+          prop: 'caseNumber1'
         },
         {
           label: '执行事项',
-          prop: 'e'
+          prop: 'item'
         },
         {
           label: '冻结股权金额',
-          prop: 'f'
+          prop: 'money'
         }
       ]
       this.loading_table = true
-      setTimeout(() => {
-        this.dialogData = []
-        this.loading_table = false
-      }, 500)
+      shares_freeze({ companyId: csfId })
+        .then((res) => {
+          this.dialogData = res.result || []
+          this.loading_table = false
+        })
+        .catch((err) => {
+          this.dialogData = []
+          this.loading_table = false
+        })
     }
   }
 }
